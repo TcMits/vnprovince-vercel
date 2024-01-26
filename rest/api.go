8853520bf1465fa4bb69f9dc95b6f80748518c78
Lâm Hoàng Phúc <64113128+TcMits@users.noreply.github.com>
2024-01-26 17:16:43 +0700

    public api

---
+++ rest/api.go
@@ -0,0 +1,130 @@
+package rest
+
+import (
+	"context"
+	"errors"
+	"strconv"
+
+	"github.com/TcMits/aipstr"
+	"github.com/TcMits/vnprovince"
+	"github.com/TcMits/vnprovince-vercel/rest/proto"
+	"github.com/alecthomas/participle/v2"
+	"go.einride.tech/aip/pagination"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+var stopIteration = errors.New("stop iteration")
+
+func getIDFromDivision(d *vnprovince.Division) int64 {
+  return int64(d.ProvinceCode + d.DistrictCode + d.WardCode)
+}
+
+type VNProvinceService struct {
+	proto.UnimplementedVNProvinceServiceServer
+
+	divisionDeclaration *aipstr.Declaration[selector]
+	parser              *participle.Parser[aipstr.Filter]
+}
+
+func NewVNProvinceService() *VNProvinceService {
+	return &VNProvinceService{
+		divisionDeclaration: getFilterDeclaration(),
+		parser:              aipstr.NewFilterParser(),
+	}
+}
+
+func apiDivisionFromDivision(dst *proto.Division, src *vnprovince.Division) {
+	dst.Id = int32(getIDFromDivision(src))
+	dst.Name = proto.DivisionResourceName{DivisionId: strconv.FormatInt(getIDFromDivision(src), 10)}.String()
+	dst.ProvinceCode = int32(src.ProvinceCode)
+	dst.DistrictCode = int32(src.DistrictCode)
+	dst.WardCode = int32(src.WardCode)
+	dst.ProvinceName = src.ProvinceName
+	dst.DistrictName = src.DistrictName
+	dst.WardName = src.WardName
+}
+
+func (s *VNProvinceService) ListDivisions(ctx context.Context, req *proto.ListDivisionsRequest) (*proto.ListDivisionsResponse, error) {
+	if err := req.Validate(); err != nil {
+		return nil, status.Error(codes.InvalidArgument, err.Error())
+	}
+
+	filter := trueSelector
+	if filterStr := req.GetFilter(); filterStr != "" {
+		filterStruct, err := s.parser.ParseString("", filterStr)
+		if err != nil {
+			return nil, status.Error(codes.InvalidArgument, err.Error())
+		}
+
+		filter, err = s.divisionDeclaration.WhereClause(filterStruct)
+		if err != nil {
+			return nil, status.Error(codes.InvalidArgument, err.Error())
+		}
+	}
+
+	pageToken, _ := pagination.ParsePageToken(req)
+	pageSize := req.GetPageSize()
+	resp := proto.ListDivisionsResponse{Divisions: make([]*proto.Division, 0, pageSize)}
+	index := 0
+	toOffset := int(int32(pageToken.Offset) + pageSize)
+	offset := int(pageToken.Offset)
+
+	if err := vnprovince.EachDivision(func(d vnprovince.Division) error {
+		if !filter(&d) {
+			return nil
+		}
+
+		if index >= offset && index < toOffset {
+			division := proto.Division{}
+			apiDivisionFromDivision(&division, &d)
+			resp.Divisions = append(resp.Divisions, &division)
+		}
+
+		if index >= toOffset {
+			resp.NextPageToken = pageToken.Next(req).String()
+			return stopIteration
+		}
+
+		index += 1
+		return nil
+	}); err != nil && !errors.Is(err, stopIteration) {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return &resp, nil
+}
+
+func (s *VNProvinceService) GetDivision(ctx context.Context, req *proto.GetDivisionRequest) (*proto.Division, error) {
+	if err := req.Validate(); err != nil {
+		return nil, status.Error(codes.InvalidArgument, err.Error())
+	}
+
+	rn := proto.DivisionResourceName{}
+	if err := errors.Join(rn.UnmarshalString(req.GetName()), rn.Validate()); err != nil {
+		return nil, status.Error(codes.InvalidArgument, err.Error())
+	}
+
+	expectedId, err := strconv.ParseInt(rn.DivisionId, 10, 64)
+	if err != nil {
+		return nil, status.Error(codes.InvalidArgument, err.Error())
+	}
+
+	resp := proto.Division{}
+	if err := vnprovince.EachDivision(func(d vnprovince.Division) error {
+		if getIDFromDivision(&d) == expectedId {
+			apiDivisionFromDivision(&resp, &d)
+			return stopIteration
+		}
+
+		return nil
+	}); err != nil && !errors.Is(err, stopIteration) {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	if resp.Id == 0 {
+		return nil, status.Error(codes.NotFound, "not found")
+	}
+
+	return &resp, nil
+}
